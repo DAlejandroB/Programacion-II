62a0c457d008b30d4547b191a4957f2052d93e71
Andrés Felipe Chaparro <36827424+AndresFelipeChaparro@users.noreply.github.com>
2018-04-01 22:57:42 -0500

    Add files via upload

--- Andrés Felipe Chaparro Rosas/guicanvas_v0/src/com/afcr/graphics/gui/Figure.java
+++ Andrés Felipe Chaparro Rosas/guicanvas_v0/src/com/afcr/graphics/gui/Figure.java
@@ -1,18 +1,42 @@
 package com.afcr.graphics.gui;
 
 import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.AffineTransform;
 
+/**
+ * Clase Figure
+ * 
+ * @author Andrés Felipe Chaparro Rosas
+ * @since 1.8
+ */
 public class Figure {
 
-	private Graphics graphics;
 	private int size;
+	private double rotation;
+	private Point point;
+	private Graphics2D graphics;
 
-	public Figure(int size) {
+	/**
+	 * Constructor que inicializa los atributos size y point
+	 * 
+	 * @param size
+	 *            tamaño de la bicicleta
+	 * @param point
+	 *            posicion de la bicicleta
+	 */
+	public Figure(int size, Point point, double rotation) {
 		this.size = size;
+		this.point = point;
+		this.rotation = rotation;
 	}
 
+	/**
+	 * Muestra el dibujo de una bicicleta
+	 */
 	public void show() {
+		rotationAction();
 		wheels();
 		armchair();
 		skeleton();
@@ -21,73 +45,111 @@ public class Figure {
 		pedals();
 	}
 
-	private void wheels() {
+	private void handle() {
 		graphics.setColor(Color.black);
-		graphics.fillOval(450, 400, 100, 100);
-		graphics.fillOval(700, 400, 100, 100);
-		graphics.setColor(Color.white);
-		graphics.fillOval(460, 410, 80, 80);
-		graphics.fillOval(710, 410, 80, 80);
+		int[] x1 = { this.point.x + 295 * this.size / 100, this.point.x + 305 * this.size / 100,
+				this.point.x + 275 * this.size / 100, this.point.x + 265 * this.size / 100 };
+		int[] y1 = { this.point.y + 50 * this.size / 100, this.point.y + 50 * this.size / 100,
+				this.point.y - 100 * this.size / 100, this.point.y - 100 * this.size / 100 };
+		graphics.fillPolygon(x1, y1, 4);
+		int[] x2 = { this.point.x + 220 * this.size / 100, this.point.x + 220 * this.size / 100,
+				this.point.x + 320 * this.size / 100, this.point.x + 320 * this.size / 100 };
+		int[] y2 = { this.point.y - 100 * this.size / 100, this.point.y - 110 * this.size / 100,
+				this.point.y - 100 * this.size / 100, this.point.y - 90 * this.size / 100 };
+		graphics.fillPolygon(x2, y2, 4);
 	}
 
 	private void armchair() {
 		graphics.setColor(Color.black);
-		int[] x1 = { 590, 580, 590, 600 };
-		int[] y1 = { 325, 325, 460, 460 };
+		int[] x1 = { this.point.x + 14 * this.size / 10, this.point.x + 13 * this.size / 10,
+				this.point.x + 14 * this.size / 10, this.point.x + 15 * this.size / 10 };
+		int[] y1 = { this.point.y - 75 * this.size / 100, this.point.y - 75 * this.size / 100,
+				this.point.y + 6 * this.size / 10, this.point.y + 6 * this.size / 10 };
 		graphics.fillPolygon(x1, y1, 4);
 		graphics.setColor(Color.blue);
-		int[] x2 = { 565, 630, 565};
-		int[] y2 = { 312, 325, 345};
+		int[] x2 = { this.point.x + 23 * this.size / 20, this.point.x + 18 * this.size / 10,
+				this.point.x + 23 * this.size / 20 };
+		int[] y2 = { this.point.y - 88 * this.size / 100, this.point.y - 75 * this.size / 100,
+				this.point.y - 55 * this.size / 100 };
 		graphics.fillPolygon(x2, y2, 3);
-	}
-
-	private void skeleton() {
-		graphics.setColor(Color.red);
-		int[] x1 = { 500, 510, 600, 590 };
-		int[] y1 = { 450, 450, 360, 360 };
-		graphics.fillPolygon(x1, y1, 4);
-		int[] x2 = { 500, 510, 600, 590 };
-		int[] y2 = { 450, 460, 470, 460 };
-		graphics.fillPolygon(x2, y2, 4);
-		int[] x3 = { 600, 590, 715, 725 };
-		int[] y3 = { 470, 460, 325, 325 };
-		graphics.fillPolygon(x3, y3, 4);
-		int[] x4 = { 715, 715, 590, 590 };
-		int[] y4 = { 325, 335, 370, 360 };
-		graphics.fillPolygon(x4, y4, 4);
-	}
-
-	private void pinions() {
-		graphics.setColor(Color.black);
-		graphics.fillOval(490, 440, 20, 20);
-		graphics.fillOval(740, 440, 20, 20);
-	}
-
-	private void handle() {
-		graphics.setColor(Color.black);
-		int[] x1 = { 745, 755, 725, 715};
-		int[] y1 = { 450, 450, 300, 300 };
-		graphics.fillPolygon(x1, y1, 4);
-		int[] x2 = { 670, 670, 770, 770 };
-		int[] y2 = { 300, 290, 300, 310 };
-		graphics.fillPolygon(x2, y2, 4);
 	}
 
 	private void pedals() {
 		graphics.setColor(Color.black);
-		graphics.fillOval(575, 440, 40, 40);
-		int[] x1 = {620, 630, 570, 560};
-		int[] y1 = {490, 490, 430, 430};
+		graphics.fillOval(this.point.x + 25 * this.size / 20, this.point.y + this.size * 2 / 5, this.size * 2 / 5,
+				this.size * 2 / 5);
+		int[] x1 = { this.point.x + this.size * 17 / 10, this.point.x + 18 * this.size / 10,
+				this.point.x + 12 * this.size / 10, this.point.x + 11 * this.size / 10 };
+		int[] y1 = { this.point.y + 9 * this.size / 10, this.point.y + 9 * this.size / 10,
+				this.point.y + 3 * this.size / 10, this.point.y + 3 * this.size / 10 };
 		graphics.fillPolygon(x1, y1, 4);
-		int[] x2 = { 615, 635, 635, 615 };
-		int[] y2 = { 493, 493, 488, 488 };
+		int[] x2 = { this.point.x + 33 * this.size / 20, this.point.x + 37 * this.size / 20,
+				this.point.x + 37 * this.size / 20, this.point.x + 33 * this.size / 20 };
+		int[] y2 = { this.point.y + 93 * this.size / 100, this.point.y + 93 * this.size / 100,
+				this.point.y + 88 * this.size / 100, this.point.y + 88 * this.size / 100 };
 		graphics.fillPolygon(x2, y2, 4);
-		int[] x3 = { 575, 555, 555, 575 };
-		int[] y3 = { 433, 433, 428, 428 };
+		int[] x3 = { this.point.x + 25 * this.size / 20, this.point.x + 21 * this.size / 20,
+				this.point.x + 21 * this.size / 20, this.point.x + 25 * this.size / 20 };
+		int[] y3 = { this.point.y + 33 * this.size / 100, this.point.y + 33 * this.size / 100,
+				this.point.y + 28 * this.size / 100, this.point.y + 28 * this.size / 100 };
 		graphics.fillPolygon(x3, y3, 4);
 	}
-	
-	public void setGraphics(Graphics graphics) {
+
+	private void skeleton() {
+		graphics.setColor(Color.red);
+		int[] x1 = { this.point.x + this.size / 2, this.point.x + this.size * 3 / 5, this.point.x + this.size * 3 / 2,
+				this.point.x + this.size * 7 / 5 };
+		int[] y1 = { this.point.y + this.size / 2, this.point.y + this.size / 2, this.point.y - this.size * 2 / 5,
+				this.point.y - this.size * 2 / 5 };
+		graphics.fillPolygon(x1, y1, 4);
+		int[] x2 = { this.point.x + this.size / 2, this.point.x + this.size * 3 / 5, this.point.x + this.size * 3 / 2,
+				this.point.x + this.size * 7 / 5 };
+		int[] y2 = { this.point.y + this.size / 2, this.point.y + this.size * 3 / 5, this.point.y + this.size * 7 / 10,
+				this.point.y + this.size * 3 / 5 };
+		graphics.fillPolygon(x2, y2, 4);
+		int[] x3 = { this.point.x + this.size * 3 / 2, this.point.x + this.size * 7 / 5,
+				this.point.x + this.size * 53 / 20, this.point.x + this.size * 55 / 20 };
+		int[] y3 = { this.point.y + this.size * 7 / 10, this.point.y + this.size * 3 / 5,
+				this.point.y - this.size * 15 / 20, this.point.y - this.size * 15 / 20 };
+		graphics.fillPolygon(x3, y3, 4);
+		int[] x4 = { this.point.x + this.size * 53 / 20, this.point.x + this.size * 53 / 20,
+				this.point.x + this.size * 7 / 5, this.point.x + this.size * 7 / 5 };
+		int[] y4 = { this.point.y - this.size * 15 / 20, this.point.y - this.size * 13 / 20,
+				this.point.y - this.size * 3 / 10, this.point.y - this.size * 2 / 5 };
+		graphics.fillPolygon(x4, y4, 4);
+	}
+
+	private void wheels() {
+		graphics.setColor(Color.black);
+		graphics.fillOval(this.point.x, this.point.y, this.size, this.size);
+		graphics.fillOval(this.point.x + (this.size * 25 / 10), this.point.y, this.size, this.size);
+		graphics.setColor(Color.white);
+		graphics.fillOval(this.point.x + (this.size / 10), this.point.y + (this.size / 10), this.size * 4 / 5,
+				this.size * 4 / 5);
+		graphics.fillOval(this.point.x + (this.size * 25 / 10) + (this.size / 10), this.point.y + (this.size / 10),
+				this.size * 4 / 5, this.size * 4 / 5);
+	}
+
+	private void pinions() {
+		graphics.setColor(Color.black);
+		graphics.fillOval(this.point.x + this.size * 2 / 5, this.point.y + this.size * 2 / 5, this.size * 1 / 5,
+				this.size * 1 / 5);
+		graphics.fillOval(this.point.x + (this.size * 29 / 10), this.point.y + this.size * 2 / 5, this.size * 1 / 5,
+				this.size * 1 / 5);
+	}
+
+	private void rotationAction() {
+		this.graphics.transform(AffineTransform.getRotateInstance(Math.toRadians(this.rotation),
+				this.point.x+175* this.size / 100, this.point.y-this.size / 20));
+	}
+
+	/**
+	 * Establece el valor del objeto graphics
+	 * 
+	 * @param graphics
+	 *            objeto que se utiliza para los diversos dibujos
+	 */
+	public void setGraphics(Graphics2D graphics) {
 		this.graphics = graphics;
 	}
 }
