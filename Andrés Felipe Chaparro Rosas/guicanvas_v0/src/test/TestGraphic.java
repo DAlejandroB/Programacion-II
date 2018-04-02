68543d5125c61e9519afdd1e1ea56635e598600b
Andrés Felipe Chaparro <36827424+AndresFelipeChaparro@users.noreply.github.com>
2018-04-01 22:58:07 -0500

    Add files via upload

--- Andrés Felipe Chaparro Rosas/guicanvas_v0/src/test/TestGraphic.java
+++ Andrés Felipe Chaparro Rosas/guicanvas_v0/src/test/TestGraphic.java
@@ -1,12 +1,14 @@
 package test;
 
+import java.awt.Point;
+
 import com.afcr.graphics.gui.Figure;
 import com.afcr.graphics.gui.JFrameAppGraphic;
 
 public class TestGraphic {
 
 	public static void main(String[] args) {
-		Figure figure = new Figure(1000);
-		JFrameAppGraphic jFrameAppGraphic = new JFrameAppGraphic(figure);
+		Figure figure = new Figure(100, new Point(450, 300), 0);
+		 new JFrameAppGraphic(figure);
 	}
 }
